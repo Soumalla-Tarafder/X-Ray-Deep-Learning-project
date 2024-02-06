a0e5b8b74a8f744951e4064fd2e325f01e379773
soumalla <soumallatarafder@gmail.com>
2024-02-06 21:50:30 +0530

    initial commit for Xray Deep learning project

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ setup(
 
 name="Xray",
 version="0.0.1",
-author="sunny savita",
-author_email="sunny.savita@ineuron.ai",
-install_requires=get_requirements(r"C:\\Users\\sunny\\deeplearningproject\\requirements_dev.txt"),
+author="Soumalla-Tarafder",
+author_email="soumallatarafder@gmail.com",
+install_requires=get_requirements(r"C:\Users\SOUMALLA-ANTARA\OneDrive\Desktop\ALL Projects\deeplearningproject\requirements_dev.txt"),
 package=find_packages()
 
 )
