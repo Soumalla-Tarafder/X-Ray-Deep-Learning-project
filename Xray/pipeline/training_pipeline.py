e129d31241d17a38affe5fa0be9c1b5f57f82f9e
soumalla <soumallatarafder@gmail.com>
2024-02-26 01:08:47 +0530

    model trainer component complete

--- Xray/pipeline/training_pipeline.py
+++ Xray/pipeline/training_pipeline.py
@@ -1,6 +1,7 @@
 import sys
 from Xray.components.data_ingestion import DataIngestion
 from Xray.components.data_transformation import DataTransformation
+from Xray.components.model_training import ModelTrainer
 from Xray.entity.artifact_entity import * 
 from Xray.entity.config_entity import *
 from Xray.exception import XRayException
@@ -10,6 +11,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config=DataIngestionConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
         
     def start_data_ingestion(self) -> DataIngestionArtifact:
         logging.info("Entered the start_data_ingestion method of TrainPipeline class")
@@ -59,6 +61,24 @@ class TrainPipeline:
         except Exception as e:
             raise XRayException(e, sys)
         
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        logging.info("Entered the start_model_trainer method of TrainPipeline class")
+
+        try:
+            model_trainer = ModelTrainer(
+                data_transformation_artifact=data_transformation_artifact,
+                model_trainer_config=self.model_trainer_config,
+            )
+
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+
+            logging.info("Exited the start_model_trainer method of TrainPipeline class")
+
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise XRayException(e, sys)
+        
 
 
 
@@ -72,7 +92,9 @@ class TrainPipeline:
                     data_ingestion_artifact=data_ingestion_artifact
                 )
             )
-
+            model_trainer_artifact: ModelTrainerArtifact = self.start_model_trainer(
+                data_transformation_artifact=data_transformation_artifact
+            )
 
             logging.info("Exited the run_pipeline method of TrainPipeline class")
 
